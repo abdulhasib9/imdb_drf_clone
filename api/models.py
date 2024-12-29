c91c25c262cac0b4d46b023eed261fc9fd4538f7
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-12-29 13:28:10 -0800

    added watchlist model and streaming

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=50)
+    about = models.CharField(max_length=200)
+    website = models.URLField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Watchlist(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
