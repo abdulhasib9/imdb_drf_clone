fab82cb0181cf5ec7c4ae74432b7f91a1d9d5d76
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-12-29 13:33:19 -0800

    added watchlistav class

--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from .models import Watchlist,StreamPlatform
+from rest_framework.views import APIView
 
-
+class WatchlistAV(APIView):
+    def get(self,request):
+        watchlists = Watchlist.objects.all()
+        data = list(watchlists.values())
+        return Response(data)
