e397a43396a2cd47ddeffe039d2dfd1523c83a28
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-12-30 07:25:41 -0800

    added review list mixin views and review detail mixin generic views and urls

--- api/urls.py
+++ api/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('watchlist/',views.WatchlistAV.as_view()),
     path('movie/<int:pk>/',views.WatchlistDetailAV.as_view()),
     path('platforms/',views.StreamPlatformAV.as_view()),
-    path('platform/<int:pk>/',views.StreamPlatformDetailAV.as_view())
+    path('platform/<int:pk>/',views.StreamPlatformDetailAV.as_view()),
+    path('reviews/',views.ReviewList.as_view()),
+    path('review/<int:pk>/',views.ReviewDetail.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,12 +1,33 @@
+#app imports
 from django.shortcuts import render
-from rest_framework.response import Response
-from .models import Watchlist,StreamPlatform
-from rest_framework.views import APIView
-from .serializers import WatchlistSerializer,StreamPlatformSerializer
+from .models import Watchlist,StreamPlatform,Review
+from .serializers import WatchlistSerializer,StreamPlatformSerializer,ReviewSerializer
+
+#rest framework imports
 from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
+class ReviewList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+    def get (self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
+    def post (self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
 
+
+class ReviewDetail(mixins.RetrieveModelMixin,generics.GenericAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    def get (self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
 
 class StreamPlatformAV(APIView):
     def get(self,request):
