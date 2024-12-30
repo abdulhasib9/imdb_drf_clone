a3240400e8df963c099c77a1f997abb4090a6fbe
abdul hasib yousufzai <yousufzai.abdulhasib@gmail.com>
2024-12-29 22:52:24 -0800

    added watchlist field in streamplatformserializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,14 @@ from rest_framework.serializers import ModelSerializer
 from .models import Watchlist,StreamPlatform
 
 class WatchlistSerializer(ModelSerializer):
+    
     class Meta:
         model =Watchlist
         fields ="__all__"
         
 
 class StreamPlatformSerializer(ModelSerializer):
+    watchlist = WatchlistSerializer(many=True,read_only=True)
     class Meta:
         model = StreamPlatform
         fields ='__all__'
